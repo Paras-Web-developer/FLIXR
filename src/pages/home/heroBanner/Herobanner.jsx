b6a8d3fbc9bdc6c8145373f488812688edcd04ad
Paras Grover <parasgrover932@gmail.com>
2023-06-25 18:59:39 +0530

    final update

--- src/pages/home/heroBanner/Herobanner.jsx
+++ src/pages/home/heroBanner/Herobanner.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import useFetch from "../../../hooks/useFetch";
-// import Img from "../../../components/lazyLoadImage/img";
+import Img from "../../../components/lazyLoadImage/img"
 import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
 import "./style.scss";
 export default function Herobanner() {
